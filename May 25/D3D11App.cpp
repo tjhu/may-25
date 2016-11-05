bd169fa36c970b540cc9df106b373aae354ffd29
Tianjiao Huang <maitian519@gmail.com>
2016-11-05 14:00:03 -0700

    Attempt to change framerate but failed

--- May 25/D3D11App.cpp
+++ May 25/D3D11App.cpp
@@ -81,7 +81,7 @@ void InitDevice(HWND hWnd, HINSTANCE hInstance)
 	sd.BufferDesc.Width = width;
 	sd.BufferDesc.Height = height;
 	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Numerator = 30;
 	sd.BufferDesc.RefreshRate.Denominator = 1;
 	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	sd.OutputWindow = hWnd;

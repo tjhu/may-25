de53bca0d12c41800481dc2394cc8f11322dd844
Tianjiao Huang <maitian519@gmail.com>
2017-02-06 22:46:02 -0800

    Translate MathML to infix

--- MathML/main.cpp
+++ MathML/main.cpp
@@ -1,33 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include "alglib/integration.h"
+#include <Windows.h>
+#include <iostream>
+#import "C:\Users\maiti\Source\Repos\May 25\CSharpSBML\bin\Debug\CCR.tlb" no_namespace
 
-using namespace alglib;
-void int_function_1_func(double x, double xminusa, double bminusx, double &y, void *ptr)
+
+int main(void)
 {
-	// this callback calculates f(x)=exp(x)
-	y = sin(x);
-}
+	CoInitialize(NULL);
 
-int main(int argc, char **argv)
-{
-	//
-	// This example demonstrates integration of f=exp(x) on [0,1]:
-	// * first, autogkstate is initialized
-	// * then we call integration function
-	// * and finally we obtain results with autogkresults() call
-	//
-	double a = 0;
-	double b = 3;
-	autogkstate s;
-	double v;
-	autogkreport rep;
+	IMyClassPtr obj;
+	obj.CreateInstance(__uuidof(MyClass));
+	std::cout << obj->GetData() << std::endl;
 
-	autogksmooth(a, b, s);
-	alglib::autogkintegrate(s, int_function_1_func);
-	autogkresults(s, v, rep);
-
-	printf("%.15f\n", double(v)); // EXPECTED: 1.7182
+	CoUninitialize();
 	return 0;
 }
